5bfaed8e9a493a45889a655e2b0aa89bfa41a71a
GlushkovVK <glushkov.vk@students.dvfu.ru>
2019-11-25 23:41:53 +1000

    Add empty event static constructor

--- app/src/main/java/com/truefinch/enceladus/models/EventModel.kt
+++ app/src/main/java/com/truefinch/enceladus/models/EventModel.kt
@@ -1,20 +1,35 @@
 package com.truefinch.enceladus.models
 
 import android.os.Parcelable
+import android.util.EventLogTags
 import androidx.room.Entity
+import com.truefinch.enceladus.ui.eventFragment.EventViewModel
+import com.truefinch.enceladus.utils.toZoneDateTime
 import kotlinx.android.parcel.Parcelize
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.util.*
 
 @Entity
 data class EventModel internal constructor(
     val id: Long,
     val title: String?,
     val location: String?,
+    val description: String?,
     val pattern_id: Long,
     var start_date: ZonedDateTime,
     var end_date: ZonedDateTime
 ) {
+    companion object {
+        fun emptyEvent(): EventModel =
+            emptyEvent(ZonedDateTime.now())
+
+        fun emptyEvent(calendar: Calendar) =
+            emptyEvent(toZoneDateTime(calendar))
+
+        fun emptyEvent(zonedDateTime: ZonedDateTime): EventModel =
+            EventModel(-1, "", "", "", -1, zonedDateTime, zonedDateTime.plusHours(1))
+    }
 
     init {
         start_date = start_date.withZoneSameInstant(ZoneId.systemDefault())
